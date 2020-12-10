87da2e99a926e7a85af478147a37552580621711
Andy Cavatorta <hellyeah@media.mit.edu>
2020-12-10 16:25:48 -0500

    tests

--- roles/dervishes-1/main.py
+++ roles/dervishes-1/main.py
@@ -89,3 +89,4 @@ class Main(threading.Thread):
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 print(e, repr(traceback.format_exception(exc_type, exc_value,exc_traceback)))
 
+main = Main()
